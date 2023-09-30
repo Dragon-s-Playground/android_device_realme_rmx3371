e69e8dfeaee9b3c7fdd0e44b1d53f56981e8471f
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2023-09-30 15:52:54 +0530

    spartan: build thermal service

--- device.mk
+++ device.mk
@@ -387,6 +387,11 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oplus
 
+#Thermals
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor
+    
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
