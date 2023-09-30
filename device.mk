5745b60fff8988848c23f5a206ce0b517ed889dd
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2023-09-30 20:27:39 +0530

    spartan: nuke audio_amplifer.kona

--- device.mk
+++ device.mk
@@ -50,7 +50,6 @@ PRODUCT_PACKAGES += \
     audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
-    audio_amplifier.kona \
     liba2dpoffload \
     libbatterylistener \
     libcomprcapture \
